a2fa00cfa285221387d9231b7c06a9b04a4dc1ac
edoardo.fantolino <68923497+edoardofantolino@users.noreply.github.com>
2021-08-20 21:49:59 +0200

    Update voc.py

--- dataset/voc.py
+++ dataset/voc.py
@@ -136,7 +136,7 @@ class VOCSegmentationIncremental(data.Dataset):
                 idxs = np.load(idxs_path).tolist()
             else:
                 idxs = filter_images(full_voc, labels, labels_old, overlap=overlap)
-                if idxs_path is not None and distributed.get_rank() == 0:
+                if idxs_path is not None:
                     np.save(idxs_path, np.array(idxs, dtype=int))
 
             if train:
