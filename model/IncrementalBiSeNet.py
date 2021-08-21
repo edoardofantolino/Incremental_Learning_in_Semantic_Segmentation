dd434cbdb6e52f619bee8c3532b526d0e0dc9aeb
edoardo.fantolino <68923497+edoardofantolino@users.noreply.github.com>
2021-08-21 04:08:35 +0200

    Update IncrementalBiSeNet.py

--- model/IncrementalBiSeNet.py
+++ model/IncrementalBiSeNet.py
@@ -1,197 +1,257 @@
 import torch
-from torch import nn
-from model.build_contextpath import build_contextpath
-import warnings
-warnings.filterwarnings(action='ignore')
+from torch import distributed
+import torch.nn as nn
+from apex import amp
+from functools import reduce
 
-class ConvBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2,padding=1):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
-
-    def forward(self, input):
-        x = self.conv1(input)
-        return self.relu(self.bn(x))
-
-class Spatial_path(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.convblock1 = ConvBlock(in_channels=3, out_channels=64)
-        self.convblock2 = ConvBlock(in_channels=64, out_channels=128)
-        self.convblock3 = ConvBlock(in_channels=128, out_channels=256)
-
-    def forward(self, input):
-        x = self.convblock1(input)
-        x = self.convblock2(x)
-        x = self.convblock3(x)
-        return x
-
-class AttentionRefinementModule(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.sigmoid = nn.Sigmoid()
-        self.in_channels = in_channels
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-
-    def forward(self, input):
-        # global average pooling
-        x = self.avgpool(input)
-        assert self.in_channels == x.size(1), 'in_channels and out_channels should all be {}'.format(x.size(1))
-        x = self.conv(x)
-        x = self.sigmoid(self.bn(x))
-        #x = self.sigmoid(x)
-        # channels of input and x should be same
-        x = torch.mul(input, x)
-        return x
+from utils.loss import KnowledgeDistillationLoss, BCEWithLogitsLossWithIgnoreIndex, \
+    UnbiasedKnowledgeDistillationLoss, UnbiasedCrossEntropy, IcarlLoss
+from utils import get_regularizer
 
 
-class FeatureFusionModule(torch.nn.Module):
-    def __init__(self, num_classes, in_channels):
-        super().__init__()
-        # self.in_channels = input_1.channels + input_2.channels
-        # resnet101 3328 = 256(from context path) + 1024(from spatial path) + 2048(from spatial path)
-        # resnet18  1024 = 256(from context path) + 256(from spatial path) + 512(from spatial path)
-        self.in_channels = in_channels
+class Trainer:
+    def __init__(self, model, model_old, device, opts, trainer_state=None, classes=None):
 
-        self.convblock = ConvBlock(in_channels=self.in_channels, out_channels=num_classes, stride=1)
-        self.conv1 = nn.Conv2d(num_classes, num_classes, kernel_size=1)
-        self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(num_classes, num_classes, kernel_size=1)
-        self.sigmoid = nn.Sigmoid()
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.model_old = model_old
+        self.model = model
+        self.device = device
 
-
-    def forward(self, input_1, input_2):
-        x = torch.cat((input_1, input_2), dim=1)
-        assert self.in_channels == x.size(1), 'in_channels of ConvBlock should be {}'.format(x.size(1))
-        feature = self.convblock(x)
-        x = self.avgpool(feature)
-
-        x = self.relu(self.conv1(x))
-        x = self.sigmoid(self.conv2(x))
-        x = torch.mul(feature, x)
-        x = torch.add(x, feature)
-        return x
-
-class IncrementalBiSeNet(torch.nn.Module):
-    def __init__(self, classes, context_path):
-        # classes is a list where to every index corresponds the num of classes for that task
-        super().__init__()
-        self.classes = classes
-        # build spatial path
-        self.spatial_path = Spatial_path()
-
-        # build context path
-        self.context_path = build_contextpath(name=context_path)
-
-        # variable to change to understand which value is better as in_channels of the last classifier
-        head_channels = 32
-
-        # build attention refinement module  for resnet 101
-        if context_path == 'resnet101' or context_path == 'resnet50':
-            self.attention_refinement_module1 = AttentionRefinementModule(1024, 1024)
-            self.attention_refinement_module2 = AttentionRefinementModule(2048, 2048)
-            # supervision block
-            # supervision1 and supervision2 are lists
-            self.supervision1 = nn.Conv2d(in_channels=1024, out_channels=head_channels, kernel_size=1)
-            self.supervision2 = nn.Conv2d(in_channels=2048, out_channels=head_channels, kernel_size=1)
-            # build feature fusion module
-            self.feature_fusion_module = FeatureFusionModule(head_channels, 3328)
-
-        elif context_path == 'resnet18':
-            # build attention refinement module  for resnet 18
-            self.attention_refinement_module1 = AttentionRefinementModule(256, 256)
-            self.attention_refinement_module2 = AttentionRefinementModule(512, 512)
-            # supervision block
-            self.supervision1 = nn.Conv2d(in_channels=256, out_channels=head_channels, kernel_size=1)
-            self.supervision2 = nn.Conv2d(in_channels=512, out_channels=head_channels, kernel_size=1)
-            # build feature fusion module
-            self.feature_fusion_module = FeatureFusionModule(head_channels, 1024)
-
+        if classes is not None:
+            new_classes = classes[-1]
+            tot_classes = reduce(lambda a, b: a + b, classes)
+            self.old_classes = tot_classes - new_classes
         else:
-            print('Error: unsupport context_path network \n')
+            self.old_classes = 0
 
-        # build final convolution
-        self.cls = nn.ModuleList(
-            [nn.Conv2d(in_channels=head_channels, out_channels=c, kernel_size=1) for c in self.classes]
-        )
+        # Select the Loss Type
+        reduction = 'none'
 
-        self.init_weight()
+        self.bce = opts.bce or opts.icarl
+        if self.bce:
+            self.criterion = BCEWithLogitsLossWithIgnoreIndex(reduction=reduction)
+        elif opts.unce and self.old_classes != 0:
+            self.criterion = UnbiasedCrossEntropy(old_cl=self.old_classes, ignore_index=255, reduction=reduction)
+        else:
+            self.criterion = nn.CrossEntropyLoss(ignore_index=255)
+            print("NORMAL CROSS ENTROPY LOSS")
 
-        self.mul_lr = []
-        self.mul_lr.append(self.spatial_path)
-        self.mul_lr.append(self.attention_refinement_module1)
-        self.mul_lr.append(self.attention_refinement_module2)
-        self.mul_lr.append(self.supervision1)
-        self.mul_lr.append(self.supervision2)
-        self.mul_lr.append(self.feature_fusion_module)
-        self.mul_lr.append(self.cls)
+        # ILTSS
+        self.lde = opts.loss_de
+        self.lde_flag = self.lde > 0. and model_old is not None
+        self.lde_loss = nn.MSELoss()
 
-    def init_weight(self):
-        for name, m in self.named_modules():
-            if 'context_path' not in name:
-                if isinstance(m, nn.Conv2d):
-                    nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
-                elif isinstance(m, nn.BatchNorm2d):
-                    m.eps = 1e-5
-                    m.momentum = 0.1
-                    nn.init.constant_(m.weight, 1)
-                    nn.init.constant_(m.bias, 0)
+        self.lkd = opts.loss_kd
+        self.lkd_flag = self.lkd > 0. and model_old is not None
+        if opts.unkd:
+            self.lkd_loss = UnbiasedKnowledgeDistillationLoss(alpha=opts.alpha)
+        else:
+            self.lkd_loss = KnowledgeDistillationLoss(alpha=opts.alpha)
 
-    def forward(self, input, ret_intermediate=False):
-        # output of spatial path
-        sx = self.spatial_path(input)
+        # ICARL
+        self.icarl_combined = False
+        self.icarl_only_dist = False
+        if opts.icarl:
+            self.icarl_combined = not opts.icarl_disjoint and model_old is not None
+            self.icarl_only_dist = opts.icarl_disjoint and model_old is not None
+            if self.icarl_combined:
+                self.licarl = nn.BCEWithLogitsLoss(reduction='mean')
+                self.icarl = opts.icarl_importance
+            elif self.icarl_only_dist:
+                self.licarl = IcarlLoss(reduction='mean', bkg=opts.icarl_bkg)
+        self.icarl_dist_flag = self.icarl_only_dist or self.icarl_combined
 
-        # output of context path
-        cx1, cx2, tail = self.context_path(input)
+        # Regularization
+        regularizer_state = trainer_state['regularizer'] if trainer_state is not None else None
+        self.regularizer = get_regularizer(model, model_old, device, opts, regularizer_state)
+        self.regularizer_flag = self.regularizer is not None
+        self.reg_importance = opts.reg_importance
 
-        cx1 = self.attention_refinement_module1(cx1)
-        cx2 = self.attention_refinement_module2(cx2)
-        cx2 = torch.mul(cx2, tail)
-        # upsampling
-        cx1 = torch.nn.functional.interpolate(cx1, size=sx.size()[-2:], mode='bilinear')
-        cx2 = torch.nn.functional.interpolate(cx2, size=sx.size()[-2:], mode='bilinear')
-        cx = torch.cat((cx1, cx2), dim=1)
+        self.ret_intermediate = self.lde
+        self.scaler=torch.cuda.amp.GradScaler()
 
-        if ret_intermediate == True:
-            cx1_sup = self.supervision1(cx1)
-            cx2_sup = self.supervision2(cx2)
-            cx1_sup = torch.nn.functional.interpolate(cx1_sup, size=input.size()[-2:], mode='bilinear')
-            cx2_sup = torch.nn.functional.interpolate(cx2_sup, size=input.size()[-2:], mode='bilinear')
+    def train(self, cur_epoch, optim, train_loader, scheduler=None, print_int=10, logger=None):
+        """Train and return epoch loss"""
+        logger.info("Epoch %d, lr = %f" % (cur_epoch, optim.param_groups[0]['lr']))
 
-        features = self.feature_fusion_module(sx, cx)
+        device = self.device
+        model = self.model
+        criterion = self.criterion
 
-        # instead of 'output of feature fusion module' and 'upsampling'
-        out = []
-        for mod in self.cls:
-            out.append(
-                mod(
-                    torch.nn.functional.interpolate(
-                        features, scale_factor=8, mode='bilinear'
-                    )
-                )
-            )
-        result = torch.cat(out, dim=1)  # 1 is the dimension of classes
+        epoch_loss = 0.0
+        reg_loss = 0.0
+        interval_loss = 0.0
+        lkd = torch.tensor(0.)
+        lde = torch.tensor(0.)
+        l_icarl = torch.tensor(0.)
+        l_reg = torch.tensor(0.)
 
-        if ret_intermediate == True:
-            return result, cx1_sup, cx2_sup
+        # train_loader.sampler.set_epoch(cur_epoch)
 
-        return result
-    
-    def init_new_classifier(self, device):
-        cls = self.cls[-1]
-        imprinting_w = self.cls[0].weight[0]
-        bkg_bias = self.cls[0].bias[0]
+        model.train()
+        for cur_step, (images, labels) in enumerate(train_loader):
 
-        bias_diff = torch.log(torch.FloatTensor([self.classes[-1] + 1])).to(device)
+            images = images.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.long)
 
-        new_bias = (bkg_bias - bias_diff)
+            with torch.cuda.amp.autocast():
+                if (self.lde_flag or self.lkd_flag or self.icarl_dist_flag) and self.model_old is not None:
+                    with torch.no_grad():
+                        outputs_old, features_old1, feature_old2 = self.model_old(images, ret_intermediate=True)
 
-        cls.weight.data.copy_(imprinting_w)
-        cls.bias.data.copy_(new_bias)
+                optim.zero_grad()
+                outputs, features1, features2 = model(images, ret_intermediate=True)
 
-        self.cls[0].bias[0].data.copy_(new_bias.squeeze(0))
+                # xxx BCE / Cross Entropy Loss
+                loss = criterion(outputs, labels)
+                loss = loss + criterion(features1, labels)
+                loss = loss + criterion(features2, labels)
+
+                # xxx first backprop of previous loss (compute the gradients for regularization methods)
+                loss_tot = loss + lkd + lde + l_icarl
+
+#             with amp.scale_loss(loss_tot, optim) as scaled_loss:
+#                 scaled_loss.backward()
+            self.scaler.scale(loss_tot).backward()
+
+            # xxx Regularizer (EWC, RW, PI)
+            # if self.regularizer_flag:
+            #     if distributed.get_rank() == 0:
+            #         self.regularizer.update()
+            #     l_reg = self.reg_importance * self.regularizer.penalty()
+            #     if l_reg != 0.:
+            #         with amp.scale_loss(l_reg, optim) as scaled_loss:
+            #             scaled_loss.backward()
+                   
+#             optim.step()
+#             if scheduler is not None:
+#                 scheduler.step()
+
+            self.scaler.step(optim)
+            self.scaler.update()
+
+            epoch_loss += loss.item()
+            reg_loss += l_reg.item() if l_reg != 0. else 0.
+            reg_loss += lkd.item() + lde.item() + l_icarl.item()
+            interval_loss += loss.item() + lkd.item() + lde.item() + l_icarl.item()
+            interval_loss += l_reg.item() if l_reg != 0. else 0.
+
+            if (cur_step + 1) % print_int == 0:
+                interval_loss = interval_loss / print_int
+                logger.info(f"Epoch {cur_epoch}, Batch {cur_step + 1}/{len(train_loader)},"
+                            f" Loss={interval_loss}")
+                logger.debug(f"Loss made of: CE {loss}, LKD {lkd}, LDE {lde}, LReg {l_reg}")
+                # visualization
+                if logger is not None:
+                    x = cur_epoch * len(train_loader) + cur_step + 1
+                    logger.add_scalar('Loss', interval_loss, x)
+                interval_loss = 0.0
+
+        # collect statistics from multiple processes
+        epoch_loss = torch.tensor(epoch_loss).to(self.device)
+        reg_loss = torch.tensor(reg_loss).to(self.device)
+
+        # torch.distributed.reduce(epoch_loss, dst=0)
+        # torch.distributed.reduce(reg_loss, dst=0)
+
+        # if distributed.get_rank() == 0:
+        #     epoch_loss = epoch_loss / distributed.get_world_size() / len(train_loader)
+        #     reg_loss = reg_loss / distributed.get_world_size() / len(train_loader)
+
+        logger.info(f"Epoch {cur_epoch}, Class Loss={epoch_loss}, Reg Loss={reg_loss}")
+
+        return (epoch_loss, reg_loss)
+
+    def validate(self, loader, metrics, ret_samples_ids=None, logger=None):
+        """Do validation and return specified samples"""
+        metrics.reset()
+        model = self.model
+        device = self.device
+        criterion = self.criterion
+        model.eval()
+
+        class_loss = 0.0
+        reg_loss = 0.0
+        lkd = torch.tensor(0.)
+        lde = torch.tensor(0.)
+        l_icarl = torch.tensor(0.)
+        l_reg = torch.tensor(0.)
+
+        ret_samples = []
+        with torch.no_grad():
+            for i, (images, labels) in enumerate(loader):
+
+                images = images.to(device, dtype=torch.float32)
+                labels = labels.to(device, dtype=torch.long)
+
+                if (self.lde_flag or self.lkd_flag or self.icarl_dist_flag) and self.model_old is not None:
+                    with torch.no_grad():
+                        outputs_old, features_old1, features_old2 = self.model_old(images, ret_intermediate=True)
+
+                outputs, features1, features2 = model(images, ret_intermediate=True)
+
+                # xxx BCE / Cross Entropy Loss
+                if not self.icarl_only_dist:
+                    loss = criterion(outputs, labels)  # B x H x W
+                else:
+                    loss = self.licarl(outputs, labels, torch.sigmoid(outputs_old))
+
+                if self.icarl_combined:
+                    # tensor.narrow( dim, start, end) -> slice tensor from start to end in the specified dim
+                    n_cl_old = outputs_old.shape[1]
+                    # use n_cl_old to sum the contribution of each class, and not to average them (as done in our BCE).
+                    l_icarl = self.icarl * n_cl_old * self.licarl(outputs.narrow(1, 0, n_cl_old),
+                                                                  torch.sigmoid(outputs_old))
+
+                # xxx ILTSS (distillation on features or logits)
+                if self.lde_flag:
+                    lde = self.lde_loss(features['body'], features_old['body'])
+
+                if self.lkd_flag:
+                    lkd = self.lkd_loss(outputs, outputs_old)
+
+                # xxx Regularizer (EWC, RW, PI)
+                if self.regularizer_flag:
+                    l_reg = self.regularizer.penalty()
+
+                class_loss += loss.item()
+                reg_loss += l_reg.item() if l_reg != 0. else 0.
+                reg_loss += lkd.item() + lde.item() + l_icarl.item()
+
+                _, prediction = outputs.max(dim=1)
+
+                labels = labels.cpu().numpy()
+                prediction = prediction.cpu().numpy()
+                metrics.update(labels, prediction)
+
+                if ret_samples_ids is not None and i in ret_samples_ids:  # get samples
+                    ret_samples.append((images[0].detach().cpu().numpy(),
+                                        labels[0],
+                                        prediction[0]))
+
+            # collect statistics from multiple processes
+            metrics.synch(device)
+            score = metrics.get_results()
+
+            class_loss = torch.tensor(class_loss).to(self.device)
+            reg_loss = torch.tensor(reg_loss).to(self.device)
+
+            # torch.distributed.reduce(class_loss, dst=0)
+            # torch.distributed.reduce(reg_loss, dst=0)
+
+            # if distributed.get_rank() == 0:
+            #     class_loss = class_loss / distributed.get_world_size() / len(loader)
+            #     reg_loss = reg_loss / distributed.get_world_size() / len(loader)
+
+            if logger is not None:
+                logger.info(f"Validation, Class Loss={class_loss}, Reg Loss={reg_loss} (without scaling)")
+
+        return (class_loss, reg_loss), score, ret_samples
+
+    def state_dict(self):
+        state = {"regularizer": self.regularizer.state_dict() if self.regularizer_flag else None}
+
+        return state
+
+    def load_state_dict(self, state):
+        if state["regularizer"] is not None and self.regularizer is not None:
+            self.regularizer.load_state_dict(state["regularizer"])
